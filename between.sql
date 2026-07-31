7308823748155e88253cfd6df1140417472f12b5
Susmitha polisetty <susmipolisetti@gmail.com>
2026-07-31 14:04:56 +0530

    Create between.sql

---
+++ between.sql
@@ -0,0 +1,11 @@
+-- Students with Marks Between 80 and 90
+
+SELECT *
+FROM Students
+WHERE Marks BETWEEN 80 AND 90;
+
+-- Students with Age Between 20 and 22
+
+SELECT *
+FROM Students
+WHERE Age BETWEEN 20 AND 22;
