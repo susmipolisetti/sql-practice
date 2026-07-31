e9a60eca0fdf34dd3792695da496f129a6fa279a
Susmitha polisetty <susmipolisetti@gmail.com>
2026-07-31 13:46:39 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES (1, 'Susmitha', 21, 'AI & DS', 92);
+
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES (2, 'Ravi', 22, 'CSE', 85);
+
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES (3, 'Priya', 21, 'ECE', 90);
+
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES (4, 'Rahul', 23, 'IT', 78);
+
+INSERT INTO Students (StudentID, Name, Age, Department, Marks)
+VALUES (5, 'Anitha', 22, 'AI & DS', 88);
