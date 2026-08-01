3cc146657c26e813f87ba8a87c5c26f7a1f7b218
Susmitha polisetty <susmipolisetti@gmail.com>
2026-08-01 13:28:59 +0530

    Create Wildcards.sql

---
+++ Wildcards.sql
@@ -0,0 +1,23 @@
+-- Names starting with 'S'
+
+SELECT *
+FROM Students
+WHERE Name LIKE 'S%';
+
+-- Names ending with 'a'
+
+SELECT *
+FROM Students
+WHERE Name LIKE '%a';
+
+-- Names containing 'i'
+
+SELECT *
+FROM Students
+WHERE Name LIKE '%i%';
+
+-- Five-letter names
+
+SELECT *
+FROM Students
+WHERE Name LIKE '_____';
